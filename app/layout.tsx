c7d427a88dfcb921b8f65d981262952e9bc9c045
Dwi Luthfianto <dwiluthfianto72@gmail.com>
2024-10-27 17:56:35 +0700

    fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "../public/global.css";
+import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = localFont({
-  src: "fonts/GeistVF.woff",
+  src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "fonts/GeistMonoVF.woff",
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
