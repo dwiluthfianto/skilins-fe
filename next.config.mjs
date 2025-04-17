5ee3c044f680c06c39302680b637b2ff9d5fafed
Dwi Luthfianto <dwiluthfianto72@gmail.com>
2025-04-17 23:32:54 +0700

    add: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
       },
       {
-        protocol: 'https',
-        hostname: 'www.gutenberg.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lqknukzebespbggnowdh.supabase.co',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
+        protocol: "https",
+        hostname: "api.skilins.online",
+        port: "",
+        pathname: "/public/**",
       },
     ],
   },
